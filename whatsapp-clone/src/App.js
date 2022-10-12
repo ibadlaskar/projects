01d9d1696ac7cdeb3e194ca45d622a7b21806f0f
ibadlaskar <ibalaskar6@gmail.com>
2022-10-11 20:52:24 -0500

    add

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Sidebar from "./sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <div className="app">
+     <h1>Clone Whatsapp</h1>
+        
+        
+
+        <div className="app_body">
+     <Sidebar/>
+        </div>
+        </div>
   );
 }
 
---
+++ whatsapp-clone/src/sidebar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './App.css';
+
+function Sidebar() {
+  return (
+    
+    <div className="sidebar">
+     <h1>Sidebar</h1>
+     <div className="sidebar_header">
+     
+        </div>
+        <div className="sidebar_serch">
+     
+        </div>
+        <div className="sidebar_chats">
+   
+        </div>
+        </div>
+  );
+}
+
+export default Sidebar;
